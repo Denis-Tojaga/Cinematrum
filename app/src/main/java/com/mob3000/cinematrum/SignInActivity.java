59ac710c7079e9b76c8e5916a40648f30a1b751a
Denis <denis.tojaga@edu.fit.ba>
2021-09-11 09:33:30 +0200

    UI Welcome,SignIn and SignUp activities done

---
+++ app/src/main/java/com/mob3000/cinematrum/SignInActivity.java
@@ -0,0 +1,17 @@
+package com.mob3000.cinematrum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+
+    }
+}
--- app/src/main/java/com/mob3000/cinematrum/WelcomeActivity.java
+++ app/src/main/java/com/mob3000/cinematrum/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.mob3000.cinematrum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+        //removes the UI actionBar at the top of the screen
+        getSupportActionBar().hide();
+    }
+
+
+
+
+    //navigating to singInActivity
+    public void btnSignInClick(View view) {
+        Intent intent = new Intent(WelcomeActivity.this,SignInActivity.class);
+        startActivity(intent);
+    }
+
+
+    //navigating to singUpActivity
+    public void btnSignUpClick(View view) {
+        Intent intent = new Intent(WelcomeActivity.this,SignUpActivity.class);
+        startActivity(intent);
     }
 }
