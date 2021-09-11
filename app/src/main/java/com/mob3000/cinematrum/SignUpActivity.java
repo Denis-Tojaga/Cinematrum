99b37485fd23d45fbfd7d0042fc391f3971a0d68
Denis <denis.tojaga@edu.fit.ba>
2021-09-11 16:15:44 +0200

    Action bar editing done

--- app/src/main/java/com/mob3000/cinematrum/SignUpActivity.java
+++ app/src/main/java/com/mob3000/cinematrum/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.mob3000.cinematrum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -13,5 +14,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         //if we are using an Activity that is extending from AppCompatActivity we need to use getSupportActionBar()
         getSupportActionBar().setTitle(R.string.signup_action_title);
+
+        ColorDrawable color = new ColorDrawable(getResources().getColor(R.color.background_theme));
+        getSupportActionBar().setBackgroundDrawable(color);
     }
 }
